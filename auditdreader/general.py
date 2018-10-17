0660607f832608c9c307964e3caaf63c34a4ee3a
AnKano <an.shubin@outlook.com>
2018-10-17 12:11:52 +0700

    Update general.py
    Contains a function overwrite that parse username by /etc/password

--- auditdreader/general.py
+++ auditdreader/general.py
@@ -6,15 +6,18 @@ import datetime
 
 
 def uid_to_usr_str(uid):
-    # TODO: Add checking by /etc/passwd
-    id_util_string = str(subprocess.check_output(['id', str(uid)]))
-    if id_util_string:
-        # uid = 1000(student)
-        user_id_str = re.search(r'.*uid=[0-9]+[(]?(\w+)[)]?', id_util_string)
-        if user_id_str:
-            return user_id_str.groups()[0]
-        else:
-            return None
+    try:
+        user_file = open('/etc/passwd', 'r')
+        userlines = user_file.readlines()
+        user_file.close()
+        for line in userlines:
+            str = re.search(r"^[^:]*:[^:]*:%s:" % uid, line)
+            if str:
+                break
+        usr_name = re.search(r"^[^:]+", str.group()).group()
+        return usr_name
+    except IOError as e_status:
+        raise e_status
 
 def name_from_path(path):
     str = re.search(r'[/](\w+)[/]?$', path)
@@ -48,9 +51,3 @@ def get_file_info(path):
     :return: Object of FileInfo class
     """
     return FileInfo(os.stat(path))
-
-
-
-
-
-
